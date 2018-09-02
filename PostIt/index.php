492a987617ed09713483aaadb90cfba34aec65ae
Nathalie <32099186+NathalieRabell@users.noreply.github.com>
2018-09-02 22:55:37 +0200

    Add files via upload

---
+++ PostIt/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Post It!</title>
+	<link rel="stylesheet" href="ourstyle.css">
+</head>
+<body>
+	<div id="wrapper">
+		<h1>Post It væggen</h1>
+
+		<ul>
+			<li>
+				<a href="#">
+					<h2>Overskrift</h2>
+					<time>Tid</time><br>
+					<p>Indhold</p>
+					<i class="writer">Forfatter</i>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<h2>Overskrift #2</h2>
+					<time>Tid</time><br>
+					<p>Indhold</p>
+					<i class="writer">Forfatter</i>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<h2>Overskrift</h2>
+					<time>Tid</time><br>
+					<p>Indhold</p>
+					<i class="writer">Forfatter</i>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<h2>Overskrift</h2>
+					<time>Tid</time><br>
+					<p>Indhold</p>
+					<i class="writer">Forfatter</i>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<h2>Overskrift</h2>
+					<time>Tid</time><br>
+					<p>Indhold</p>
+					<i class="writer">Forfatter</i>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<h2>Overskrift</h2>
+					<time>Tid</time><br>
+					<p>Indhold</p>
+					<i class="writer">Forfatter</i>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<h2>Overskrift</h2>
+					<time>Tid</time><br>
+					<p>Indhold</p>
+					<i class="writer">Forfatter</i>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<h2>Overskrift</h2>
+					<time>Tid</time><br>
+					<p>Indhold</p>
+					<i class="writer">Forfatter</i>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<h2>Overskrift</h2>
+					<time>Tid</time><br>
+					<p>Indhold</p>
+					<i class="writer">Forfatter</i>
+				</a>
+			</li>
+		</ul>
+	</div>
+</body>
+</html>
